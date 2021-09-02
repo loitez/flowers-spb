5c45dfd526467c4e57ff5dd50f241e8f2d358df1
Anastasia <87999189+loitez@users.noreply.github.com>
2021-09-02 21:56:05 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<img src="../img/arrow.svg" alt="Влево" class="arrow arrow-left">',
+        nextArrow: '<img src="../img/arrow.svg" alt="Вправо" class="arrow arrow-right">'
+    });
+})
+
+
