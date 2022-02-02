5aed793026d05bc689df5e7506be6c6e0f74c652
Anastasia <87999189+loitez@users.noreply.github.com>
2022-02-02 19:11:03 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,4 +8,52 @@ $(function(){
     });
 })
 
+const feedbackSlider = document.querySelector('.feedback__content');
+const feedbackSliderItems = document.querySelectorAll('.feedback__card');
+const feedbackArrowLeft = document.querySelector('.feedback__arrow-left');
+const feedbackArrowRight = document.querySelector('.feedback__arrow-right');
+let index = 0;
 
+const length = feedbackSliderItems.length - 1;
+
+
+feedbackArrowLeft.addEventListener('click', () => {
+    if (index === 0) {
+        index = length;
+    } else {
+        index--;
+    }
+    feedbackSliderItems.forEach((item) => {
+        console.dir(item);
+        if (item.classList.contains('active')) {
+            item.classList.remove('active');
+        }
+        feedbackSliderItems[index].classList.add('active')
+    })
+})
+feedbackArrowRight.addEventListener('click', () => {
+    if (index < length) {
+        index++;
+    } else {
+        index = 0;
+    }
+    feedbackSliderItems.forEach((item) => {
+        if (item.classList.contains('active')) {
+            item.classList.remove('active');
+        }
+        feedbackSliderItems[index].classList.add('active')
+    })
+})
+
+
+const closeMenuBtn = document.querySelector('.menu__close');
+const menus = document.querySelectorAll('.menu__list');
+
+closeMenuBtn.addEventListener('click', () => {
+        menus.forEach((item) => {
+            item.classList.toggle('active-menu')
+        })
+        console.log('ajjak');
+
+}
+)
