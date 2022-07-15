8d1031f786dea5ee7d3fa7f01d945039c1b325e6
Anastasia <87999189+loitez@users.noreply.github.com>
2022-07-16 02:32:22 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ $(function(){
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        prevArrow: '<img src="../img/arrow.svg" alt="Влево" class="arrow arrow-left">',
-        nextArrow: '<img src="../img/arrow.svg" alt="Вправо" class="arrow arrow-right">'
+        prevArrow: '<img src="../img/arrow.png" alt="Влево" class="arrow arrow-left">',
+        nextArrow: '<img src="../img/arrow.png" alt="Вправо" class="arrow arrow-right">'
     });
 })
 
